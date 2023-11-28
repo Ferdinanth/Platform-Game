f3903b72c131f293976adcc5d477d73a028394c7
Ferdinanth <Ferdinanth98@gmail.com>
2023-11-28 12:18:33 -0500

    speed
    adsusted the speed and tried making a script that would mke the camera follow the player

--- Platform-game/Assets/script folder/Player.cs
+++ Platform-game/Assets/script folder/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Player : MonoBehaviour
+public class player : MonoBehaviour
 {
     //Animation variables
 
